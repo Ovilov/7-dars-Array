d75dfdc13475a86b4a94364d4b3be299c1517ed3
Ovilov <ovilovsaidakbar@gmail.com>
2024-02-27 11:46:52 +0500

    Array add

---
+++ app.js
@@ -0,0 +1,110 @@
+// const boyi = Number(prompt("Bo'yingizni kiriting : (sm)"))
+// const vazn = Number(prompt("Vazningizni kiriting : (kg)"))
+
+// const BMI = vazn / (boyi / 100) ** 2;
+
+// if (BMI <= 18.5) {
+//     console.log("Sizning vazningiz juda yengil . Iltimos vazn olishga harakat qiling !")
+// } else if (BMI > 18.5 && BMI <= 24.9) {
+//     console.log("Siz so'glomsiz !")
+// }  else if (BMI > 24.9 && BMI <= 29.9) {
+//     console.log("Sizda ortiqcha vazn bor . Iltimos sport bilan shug'ullaning !")
+// } else {
+//     console.log("Sizda semizlik kasalligi bor . Iltimos mutahasisga ko'rining !")
+// }
+
+////////////////////////////////////////////////////////////////////////////////
+
+// const arr = ["Ahad", "Ahmad", "Ali", "Arslon"];
+
+// arr.pop = "Alo";
+
+// console.log(arr);
+
+//////////////////////////////////////////
+
+// let arr = [1, 2, 3];
+
+// alert( arr ); // 1,2,3
+// alert( String(arr) === '1,2,3' ); // true
+
+//////////////////////////////////////////
+
+// const numbers = [1,2,3,4,5]
+
+// const result = Array.isArray(numbers)
+
+// console.log(result)
+
+////////////////////////////////////////////
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// // reversing the numbers array
+// let reversedArray = numbers.reverse();
+
+// console.log(reversedArray);
+
+// // Output: [ 5, 4, 3, 2, 1 ]
+
+//////////////////////////////////////////
+
+// let languages = ["JavaScript", "Python", "C++", "Java", "Lua"];
+
+// // reversing the order of languages array
+// let reversedArray = languages.reverse();
+
+// console.log("Reversed Array: ", reversedArray);
+
+// // modifies the original array
+// console.log("Original Array: ", languages);
+
+/////////////////////////////////////////////
+
+// // defining an array
+// let fruits = ['Apple', 'Banana', 'Grape'];
+
+// // filling every element of the array with 'Cherry'
+// fruits.fill("Cherry");
+
+// console.log(fruits);
+
+///////////////////////////////////////////
+
+// // array definition
+// let language = ["JavaScript", "Python", "C", "C++"];
+
+// // replacing element of array from index 1 to 3 by 'JavaScript'
+// language.fill("JavaScript", 1, 3);
+
+// // printing the original array
+// console.log(language);
+
+// // output: [ 'JavaScript', 'JavaScript', 'JavaScript', 'C++']
+
+/////////////////////////////////////////
+// let names = ["Saidakbar", "Abubakr", "Abror", "Samandar", "Muhammadbilol"];
+
+// const newNames = [];
+
+// for (i = 0; i < names.length; i++) {
+//   let result = names[i];
+//   newNames.push(result);
+// }
+// console.log(names);
+// console.log(newNames);
+
+////////////////////////////////////////
+
+// const number = [1 , 3 , 8 , 4 , 6 , 20 , 4 , 7, 7 , 10];
+// const newNumbers = []
+
+// // console.log(number.includes(1));
+
+// for (i = 0; i < number.length; i++) {
+//   let result = number[i];
+//   newNumbers.push(result);
+// }
+
+//////////////////////////////////////
+
